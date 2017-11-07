48a8f1dc52a4c054f37b824ca45fbeadcb7ee035
danielvillaloboshdez <danivillalobos1997@gmail.com>
2017-11-07 11:27:00 +0100

    new

--- variadic.cpp
+++ variadic.cpp
@@ -4,6 +4,12 @@
 
 int main(){
 
-  printf ("2 + 3 = %i\n", 2 + 3 );
+    int op1 = 7;
+    int op2;
+
+    printf("Operando 2: ");
+    scanf(" %i", &op2);
+
+    printf ("%i + %i = %i\n", op1, op2, op1 + op2 );
   return EXIT_SUCCESS;
 }
