c6602f39bab6aee0083007e54c6ae2198f826951
DanielVillalobosHdez <danivillalobos1997@gmail.com>
2018-02-22 20:21:36 +0100

    frase

---
+++ frase.cpp
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	char frase[] = "Tu moto alpina derrapante";
+	char *algo = frase;
+	char *salto = frase;
+	char vocal;
+		
+	system("clear");
+	while ( *salto != '\0'){
+		printf("%c", *salto);
+		salto++;
+	}
+	printf("\n");
+
+	printf("Escribe una vocal: ");
+	scanf(" %c", &vocal);
+
+	while (*algo != '\0'){
+		if(*algo == 'a' || *algo == 'e'|| *algo == 'i' || *algo == 'o' || *algo == 'u'){
+			*algo = vocal;
+			printf("%c", *algo);
+			*algo++;
+		} else {
+			printf("%c", *algo);
+			*algo++;
+		}
+	}
+	printf("\n");
+
+	return EXIT_SUCCESS;
+}
