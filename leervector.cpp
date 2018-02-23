e8295a84d3e67f139917c4a769aa5a737fa55212
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-02-23 14:07:44 +0100

    new

--- leervector.cpp
+++ leervector.cpp
@@ -4,27 +4,29 @@
 int main(){
 
     int fila_vector = 0;
-    bool acabar = false;
     double numero;
     int salto;
     double *vector = NULL;
+    char end;
 
     printf("El vector: \n");
+    scanf(" %*[()]");
     do {
+        vector = (double *) realloc(vector, (fila_vector+1) * sizeof(double));
         scanf(" %lf", &numero);
-        vector = (double *) realloc(vector, fila_vector * sizeof(double));
-        *(vector+fila_vector) = numero;
-        salto = getc(stdin);
+        vector[fila_vector] = numero;
+        /*salto = getc(stdin);
         fila_vector++;
         if (salto == '\n'){
             acabar = true;
         }
-        ungetc(acabar, stdin);
-    } while (!acabar);
+        ungetc(acabar, stdin);*/
+    } while (!scanf(" %1[)]", &end));
 
+    printf("\n\t( ");
     for(int f=0; f<fila_vector; f++)
-        printf("%4.2lf ", *(vector+f));
-    printf("\n");
+        printf("\t %4.2lf ", vector[f]);
+    printf("\t )\n");
 
     return EXIT_SUCCESS;
 }
