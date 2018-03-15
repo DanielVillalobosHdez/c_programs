b69cf2193acc717fd3f7537609b487541c0f0c95
DanielVillalobosHdez <danivillalobos1997@gmail.com>
2018-03-15 19:15:17 +0100

    ahorcado

--- ahorcado/ahorcado.cpp
+++ ahorcado/ahorcado.cpp
@@ -1,41 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <strings.h>
 
 #define N 0x100
-
+#define P 50
 
 void pintar (int letras){
+
 	printf("\t\t\tAHORCADO\n");
-	
+
 	for(int cor=0; cor<3; cor++)
 		printf("\t vida %i \t", cor);
 	printf("\n");
 
 	printf("\t\t");
-	for(int rep=0; rep<letras; rep++)
+	for(int rep=0; rep<letras-1; rep++)
 		printf("_ ");
 	printf("\n");
 }
 
-int main(){
+char *seleccionar_palabra(char *puntero = NULL){
 
-	char palabras[N]; 
+	FILE *archivo;
+	char elegida[N];
 	int letras;
-	char *palabra_escogida = NULL;
+	int ltr;
 
-	printf("Palabra: ");
-	scanf(" %s", palabras);
+	archivo = fopen(".facil.txt", "rt");
 
-	letras = strlen(palabras);
-	palabra_escogida = (char *) malloc ((letras+1) * sizeof(char));
+	while (fgets(elegida, N, archivo) !=NULL){
+			letras = strlen(elegida);
+			puntero = (char *) malloc (letras * sizeof(char));
+			strncpy(puntero, elegida, (letras+1));
+	}
+	fclose(archivo);
+	ltr= strlen(elegida);
+	pintar(ltr);
+	return puntero;
+	free(puntero);
+}
+int main(){
+	char palabra[P];
 
-	system("clear");
-	system("clear");
-	system("clear");	
 
-	pintar(letras);
+	seleccionar_palabra(palabra);
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
