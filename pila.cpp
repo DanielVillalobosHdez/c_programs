3338a3465ede6dcdd69b2905200151d3c410ce9c
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-04-26 13:49:56 +0200

    new

---
+++ pila.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 0x100
+
+typedef struct {
+    int dato[N];
+    int cima;
+} pila;
+
+void push (int dato, struct *pila, )
+int pop
+
+int main(){
+
+
+
+
+  return EXIT_SUCCESS;
+}
