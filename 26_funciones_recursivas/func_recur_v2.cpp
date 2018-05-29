0943e0ebca9a36c771a64a6d78c32dcca092e878
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-05-29 10:06:41 +0200

    funciones recursivas

---
+++ 26_funciones_recursivas/func_recur_v2.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+char imprimir(char frase[], int entero){
+    char letra = frase[entero];
+
+    printf("%c", letra);
+    entero++;
+    if(letra != '\0')
+        imprimir(frase, entero);
+}
+
+char imprimirinversa(char frase[], int entero){
+    char letra = frase[entero-1];
+
+    printf("%c", letra);
+    entero--;
+    if(entero >= 0)
+        imprimirinversa(frase, entero);
+}
+
+int main(){
+
+    char frase[] = "dabale arroz a la zorra el abad";
+    int letra = 0;
+    int inversa = sizeof(frase);
+
+    imprimir(frase, letra);
+    printf("\n");
+    imprimirinversa(frase, inversa);
+    printf("\n");
+
+    return EXIT_SUCCESS;
+}
