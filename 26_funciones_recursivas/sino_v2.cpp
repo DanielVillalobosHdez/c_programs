f9320e6d0ac31a7274ca398e2866a8af5d761f92
DanielVillalobosHdez <danivillalobos1997@gmail.com>
2018-05-29 20:00:05 +0200

    Perfect numbers

---
+++ 26_funciones_recursivas/sino_v2.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+bool tiene_un_divisor(int divisor, int posible_divisor){
+
+	int suma = 0;
+	if(posible_divisor == 0){
+		printf("%i no es perfecto\n");
+		return false;
+	}
+	if((divisor % posible_divisor) == 0){
+		suma += posible_divisor;
+		if(suma == divisor)
+			return true;
+		if(suma != divisor) {
+			printf("%i no es perfecto\n", divisor);
+			return tiene_un_divisor(divisor, posible_divisor-1);
+		}
+	}
+	
+}
+
+int main(){
+
+	int numero;	
+
+	printf("Escribe un numero: ");
+	scanf("%i", &numero);
+	
+	if(tiene_un_divisor(numero, numero-1) == true)
+		printf("El %i es un numero perfecto\n", numero);
+	return EXIT_SUCCESS;
+}
