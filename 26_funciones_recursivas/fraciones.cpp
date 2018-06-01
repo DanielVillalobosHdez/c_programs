d83c3a42e98f1e048caa56c5195f757932a5b358
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-06-01 14:28:32 +0200

    fracciones continuas

--- 26_funciones_recursivas/fraciones.cpp
+++ 26_funciones_recursivas/fraciones.cpp
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 #define MAX 0x20
 
 typedef struct {
-    int numero[MAX];
+    int data[MAX];
     int cima;
-} pila;
+} Pila;
 
-void push () {
-
+void push (int data, Pila *pila) {
+    pila->data[pila->cima++]=data;
 }
 
-void pop () {
+int pop (Pila *pila) {
+    if(pila->cima < 0)
+        return 0;
+    return pila->data[--pila->cima];
+}
 
+double fc(int *coef, int cima, int num_coef){
+    if(num_coef == cima-1)
+        return coef[num_coef];
+    return coef[num_coef] +1./fc(coef, cima, num_coef+1);
 }
 
 
 int main(){
 
-   
+    Pila coef;
+    int input;
+    bzero(&coef,sizeof(coef));
+
+    do{
+        printf("Introduce un coeficiente: ");
+        scanf(" %i\n", &input);
+        if(input > 0)
+            push(input, &coef);
+    } while(input > 0);
+
+    printf("El resultado es: %.2lf\n", fc(coef.data, coef.cima, 0));
 
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
