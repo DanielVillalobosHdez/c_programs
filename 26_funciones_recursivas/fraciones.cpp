16874fd97a5ef8f28b919349f60448fbf369492b
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-06-01 13:16:17 +0200

    factorial

---
+++ 26_funciones_recursivas/fraciones.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 0x20
+
+typedef struct {
+    int numero[MAX];
+    int cima;
+} pila;
+
+void push () {
+
+}
+
+void pop () {
+
+}
+
+
+int main(){
+
+   
+
+
+  return EXIT_SUCCESS;
+}
